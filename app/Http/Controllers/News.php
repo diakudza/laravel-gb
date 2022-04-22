5a6388922e4cffa2d37195f0f5a19b182f81ee0e
mixa <diakudza@gmail.com>
2022-04-22 18:42:18 +0300

    dz3

---
+++ app/Http/Controllers/News.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class News extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, $id = null)
+    {
+        $title = 'News';
+
+        $news = [
+            '1' => ['title' => 'new 1',
+                'text' => 'text text texttexttextv texttetextxt',
+                'date' => '12-12-2022'],
+            '2' => ['title' => 'new 2',
+                'text' => 'text text texttexttextv texttetextxt',
+                'date' => '15-10-2022'],
+            '3' => ['title' => 'new 3',
+                'text' => 'text text texttexttextv texttetextxt',
+                'date' => '01-12-2022'],
+            '4' => ['title' => 'new 4',
+                'text' => 'text text texttexttextsadadv texttetextxt',
+                'date' => '30-12-2022'],
+            '5' => ['title' => 'new 5',
+                'text' => 'tsadasdadasdgdfghdfhtextxt',
+                'date' => '20-12-2022'],
+        ];
+
+        if ($id) {
+            $news = [$news[$id]];
+        }
+
+        return view('news', ['title' => 'news', 'news' => $news]);
+    }
+}
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,49 +6,39 @@
 
 @section('content')
 
-    <div>
-        <div class="align-content-center d-flex flex-row justify-content-between">
-            <span class="fs-1">News page</span>
-            <div><a href=" {{ route('news.create') }}" class="btn btn-success ">CREATE NEW</a></div>
+<div>
+    <h1>News page</h1>
+
+    @foreach($news as $new => $val)
+        <a href="{{ route('news.show' ,['news' => $new]) }}" >{{ $new }}</a>
+    @endforeach
+
+    @if( !$single  )
+
+        @foreach($news as $new=>$val)
+
+            @include('components.cart',['title' => $val['title'], 'text' => $val['text'], 'date' => $val['date'],])
+
+
+            <br>
+        @endforeach
+    @endif
+    @if( $single  )
+        <di>
+        <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+                <h4 class="my-0 fw-normal">Free</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">{{ $single['title'] }}</h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>{{ $single['text'] }}</li>
+                    <li>{{ $single['date'] }}</li>
+                </ul>
+
+            </div>
         </div>
-        @if( !property_exists($news, 'id'))
-                <br>
-            {{ $news->links()  }}
-                <br>
-{{--            @foreach($news as $new)--}}
-{{--                <a href="{{ route('news.show' ,['news' => $new->id]) }}">{{ $new->id }}</a>--}}
-{{--            @endforeach--}}
-
-            @foreach($news as $new)
-                    @include('components.cart',[$new])
-                <br>
-            @endforeach
-        @elseif ( property_exists($news, 'id') )
-            <di>
-                <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
-                        <h4 class="my-0 fw-normal">Free</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">{{ $news->title }}</h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>{{ $news->text }}</li>
-                            <li>{{ $news->created_at }}</li>
-                        </ul>
-
-                    </div>
-                </div>
-                @else
-                    <div class="card-header py-3">
-                        <h4 class="my-0 fw-normal">Yet no news</h4>
-                    </div>
-                @endif
-
-
-
-
-    </div><br>
-    {{ $news->links()  }}
-    <br>
-
+    @endif
+    <a href=" {{ route('news.create') }}" class="btn-success">CREATE NEW</a>
+</div>
 @endsection
